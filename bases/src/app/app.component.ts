4f209778c0361a0e18bf44a403a7ad1ae7cd6040
luis vallejo <lvallejo@externos.bancokestado.cl>
2022-01-27 21:47:19 -0300

    Se agrega componente contador

--- bases/src/app/app.component.ts
+++ bases/src/app/app.component.ts
@@ -5,7 +5,4 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
-  titulo: string = 'Contador App';
-  numero: number = 10;
-}
+export class AppComponent {}
