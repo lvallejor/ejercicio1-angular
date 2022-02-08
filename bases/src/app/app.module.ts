112d7273cb9f01675dcae16cb075a3bc8109773a
luis vallejo <lvallejo@externos.bancokestado.cl>
2022-02-07 21:31:05 -0300

    ngModel y formsModule

--- bases/src/app/app.module.ts
+++ bases/src/app/app.module.ts
@@ -5,10 +5,11 @@ import { AppComponent } from './app.component';
 import { ContadorModule } from '././contador/contador.module';
 
 import { HeroesModule } from './heroes/heroes.module';
+import { DbzModule } from './dbz/dbz.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, HeroesModule, ContadorModule],
+  imports: [BrowserModule, HeroesModule, ContadorModule, DbzModule],
   providers: [],
   bootstrap: [AppComponent],
 })
