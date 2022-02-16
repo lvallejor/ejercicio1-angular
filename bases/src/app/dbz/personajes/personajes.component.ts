7147fac55e9f30d6680badce5502b943f768495b
luis vallejo <lvallejo@externos.bancokestado.cl>
2022-02-16 17:20:29 -0300

    Se agrega el componente de personajes

---
+++ bases/src/app/dbz/personajes/personajes.component.ts
@@ -0,0 +1,10 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-personajes',
+  templateUrl: './personajes.component.html',
+  styleUrls: [],
+})
+export class PersonajesComponent {
+  personajes: any[] = [];
+}
